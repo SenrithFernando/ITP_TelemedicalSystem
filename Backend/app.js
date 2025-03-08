13779bd777a976e3937b1abf843f318bb0ef263b
Senrith Fernando <115388481+SenrithFernando@users.noreply.github.com>
2025-03-08 11:55:59 +0530

    database Connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
 
-const app=express();
-export default app;
+const app = express();
+app.use(express.json());
+
+app.use("/", (req, res) => {
+    res.send("It is working db");
+});
+
+// MongoDB connection string
+const MONGO_URI = "mongodb+srv://user:OpJOqPZACOoI4okW@cluster0.pwzrm.mongodb.net/";
+
+mongoose
+    .connect(MONGO_URI) // Removed deprecated options
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(5000, () => console.log("Server running on port 5000"));
+    })
+    .catch((err) => console.log(err));
