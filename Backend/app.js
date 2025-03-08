556676cb57cfeee930824a7886ac912948a7a8d1
Senrith Fernando <115388481+SenrithFernando@users.noreply.github.com>
2025-03-08 16:28:10 +0530

    connection recreate

--- Backend/app.js
+++ Backend/app.js
@@ -8,13 +8,4 @@ app.use("/", (req, res) => {
     res.send("It is working db");
 });
 
-// MongoDB connection string
-const MONGO_URI = "mongodb+srv://user:OpJOqPZACOoI4okW@cluster0.pwzrm.mongodb.net/";
 
-mongoose
-    .connect(MONGO_URI) // Removed deprecated options
-    .then(() => {
-        console.log("Connected to MongoDB");
-        app.listen(5000, () => console.log("Server running on port 5000"));
-    })
-    .catch((err) => console.log(err));
--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,32 @@ dotenv.config({ path: './config.env' });
 import app from './app.js';
 
 
+// MongoDB connection string
+// const MONGO_URI = "mongodb+srv://user:OpJOqPZACOoI4okW@cluster0.pwzrm.mongodb.net/";
+
+// mongoose
+//     .connect(MONGO_URI) // Removed deprecated options
+//     .then(() => {
+//         console.log("Connected to MongoDB");
+//         app.listen(5000, () => console.log("Server running on port 5000"));
+//     })
+//     .catch((err) => console.log(err));
+
+
+  const db = process.env.MONGO_STRING.replace(
+      '<PASSWORD>',
+      process.env.DATABASE_PASSWORD,
+    );
+    mongoose
+      .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log('DB connection successful'));
+    
+
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
