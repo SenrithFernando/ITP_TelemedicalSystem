3de1adace176e8e26e834ce1cb217a439b3e4155
Senrith Fernando <115388481+SenrithFernando@users.noreply.github.com>
2025-03-08 10:20:14 +0530

    set up backend by installin express and creating server

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+dotenv.config({ path: './config.env' });
+import app from './app.js';
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
